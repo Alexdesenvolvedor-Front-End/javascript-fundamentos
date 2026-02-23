17dc0797ab5e1c0faa07b8485dd07b4ed21d3cbe
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-02-23 18:25:04 -0300

    Curso de JS em execução.

--- 11_Estrutura condicional else/script.js
+++ 11_Estrutura condicional else/script.js
@@ -39,3 +39,43 @@ if (c > 10) {
 } else {
     console.log("Diga não está correto!");
 }
+
+
+
+
+const interruptor = "on";
+
+if (interruptor == "on") {
+    console.log(`A lampada está ligada`);
+} else {
+    console.log(`A lampada está desligada.`);
+}
+
+
+
+const hora = new Date().getHours();
+
+if (hora < 12) {
+    console.log("Bom dia!");
+} else if (hora < 18) {
+    console.log("Boa tarde");
+} else {
+    console.log("Boa noite!");
+}
+
+
+function verificar(event) {
+    event.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let teste = document.getElementById("teste");
+
+    if (nome == "" || nome == null) {
+        document.getElementById("teste").innerHTML = ("O campo não pode ser verificado!");
+        teste.style.color = "red";
+    } else {
+        document.getElementById("teste").innerHTML = ("Parabéns, seja bem vindo.!");
+        teste.style.color = "blue";
+    }
+
+}
