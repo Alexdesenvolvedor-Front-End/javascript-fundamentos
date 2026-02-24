cbe770ccb8b90bd94a74e533d3f8ea9cfc0dcf95
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-02-24 18:33:51 -0300

    Curso de java script em execução.

--- 12_Estrutura condicional else if/script.js
+++ 12_Estrutura condicional else if/script.js
@@ -4,29 +4,92 @@
 // É possível adicionar novas condigções, como no if;
 // Assim temos a possibilidade de criar várias validações, para resolver nosso problema;
 
-
-if (1 > 2) {
-    console.log("Teste");
-
-} else if (2 > 3) {
-    console.log("Teste 2");
-
-} else if (5 > 1) {
-    console.log("Agora sim!");
-}
+// const { PureComponent } = require("react");
 
 
+// if (1 > 2) {
+//     console.log("Teste");
 
-const userName = "Alex";
+// } else if (2 > 3) {
+//     console.log("Teste 2");
 
-const userAge = 43;
+// } else if (5 > 1) {
+//     console.log("Agora sim!");
+// }
 
-if (userName === "Pedro") {
-    console.log("Bem vindo Pedro");
 
-} else if (userName === "Alex" && userAge === 43) {
-    console.log("Olá Alex, você tem 43 anos de idade!");
 
-} else {
-    console.log("Nenhuma condição aceita!");
-}
+// const userName = "Alex";
+
+// const userAge = 43;
+
+// if (userName === "Pedro") {
+//     console.log("Bem vindo Pedro");
+
+// } else if (userName === "Alex" && userAge === 43) {
+//     console.log("Olá Alex, você tem 43 anos de idade!");
+
+// } else {
+//     console.log("Nenhuma condição aceita!");
+// }
+
+// let n1 = prompt("Digite um número!");
+// n1 = parseFloat(n1);
+
+// let n2 = prompt("Digite outro número!");
+// n2 = parseFloat(n2);
+
+// alert(n1 + n2);
+
+// let n1 = prompt("Digite um número!");
+
+// n1 = parseInt(n1);
+// alert(`O número convertido foi: ${n1}`);
+
+
+// let n1 = prompt("Digite um número: ");
+// n1 = parseInt(n1);
+
+// let n2 = prompt("Digite outro número: ");
+// n2 = parseInt(n2);
+
+// console.log(`O resultado da subtração é: ${n1 - n2 }`);
+
+
+// let n1 = prompt("Digite um número: ");
+// n1 = parseInt(n1);
+
+// let n2 = prompt("Digite outro número: ");
+// n2 = parseInt(n2);
+
+// console.log(`O resultado da multiplicação é: ${n1*n2}`);
+
+
+// let n1 = prompt("Digite um número: ");
+// n1 = parseFloat(n1);
+
+// let n2 = prompt("Digite outro número: ");
+// n2 = parseFloat(n2);
+
+// let media = (n1 + n2) / 2;
+
+// console.log(`A Média total é: ${media}`);
+
+
+// let n1 = prompt("Digite um número: ");
+// n1 = parseInt(n1);
+
+// let n2 = prompt("Digite outro número: ");
+// n2 = parseInt(n2);
+
+
+// console.log(`A divisão de ${n1} por ${n2} é ${n1 % n2}`);
+
+let n1 = prompt("Digite um número: ");
+n1 = parseInt(n1);
+
+
+let n2 = prompt("Digite outro número: ");
+n2 = parseInt(n2);
+
+console.log(`O resultado da exponenciação de ${n1}  elevado por ${n2} é ${n1 ** n2}`);
