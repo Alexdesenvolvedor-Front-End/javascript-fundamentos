b60dcbb9f0309c3250f988886a7f24d6d86c44ec
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-02-25 18:37:21 -0300

    Js em execução

--- 12_Estrutura condicional else if/script.js
+++ 12_Estrutura condicional else if/script.js
@@ -85,11 +85,67 @@
 
 // console.log(`A divisão de ${n1} por ${n2} é ${n1 % n2}`);
 
-let n1 = prompt("Digite um número: ");
-n1 = parseInt(n1);
+// let n1 = prompt("Digite um número: ");
+// n1 = parseInt(n1);
 
 
-let n2 = prompt("Digite outro número: ");
-n2 = parseInt(n2);
+// let n2 = prompt("Digite outro número: ");
+// n2 = parseInt(n2);
 
-console.log(`O resultado da exponenciação de ${n1}  elevado por ${n2} é ${n1 ** n2}`);
+// console.log(`O resultado da exponenciação de ${n1}  elevado 5
+//     22por ${n2} é ${n1 ** n2}`);
+
+
+
+
+// let n1 = prompt("Digite uma nota: ");
+// n1 = parseFloat(n1);
+
+// let n2 = prompt("Digite outra nota: ");
+// n2 = parseFloat(n2);
+
+// let media = (n1 + n2) / 2;
+
+// if (media > 5) {
+//     console.log("Parabéns você foi aprovado!");
+// } else {
+//     console.log("Infelizmente você está reprovado!");
+// }
+
+
+
+let jogador1 = prompt("Jogador 1: Digite um número");
+jogador1 = parseInt(jogador1);
+
+let jogador2 = prompt("Jogador 2: Digite outro número");
+jogador2 = parseInt(jogador2);
+
+
+if (jogador1 === jogador2) {
+    console.log("Empate");
+
+} else {
+
+    let sorteado = parseInt(Math.random() * 2);
+
+    if (sorteado === 0) {
+        if (jogador1 < jogador2) {
+            console.log("Ganhou o jogador 1");
+
+        } else {
+            console.log("Gnahou o jogador 2");
+            if (jogador1 > jogador2) {
+                console.log("Ganhou o jogador 1");
+
+            } else {
+                console.log("Gangou o jogador 2");
+            }
+        }
+    }
+
+
+
+
+
+
+}
